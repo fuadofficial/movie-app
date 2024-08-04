cd37864b4d7178d5ba7eb681353d72377cc178ca
fuad <ahmadfuadoorakam@gmail.com>
2024-08-04 18:19:44 +0530

    creatign..... bc image

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home/Home"
 import Movie from "./pages/Movie/Movie"
-import  Header  from "./components/Header/Header"
+import Header from "./components/Header/Header"
 
 
 const App = () => {
-  return (
-    <div className="body">
-      <div className="project">
-        <img className="bc-img" src="../public/img/bc.jpeg" alt="" />
-        <Router>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/movie" element={<Movie />} />
-          </Routes>
-        </Router>
-      </div>
-    </div>
-  )
+    return (
+        <div className="app">
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/movie" element={<Movie />} />
+                </Routes>
+            </Router>
+        </div>
+    )
 }
 
 export default App;
